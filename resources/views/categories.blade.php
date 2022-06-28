37930de9905ebdf1ae50ea1398e52b10e8316e41
jeancgarciaq <jcvictory@hotmail.com>
2022-06-28 15:10:21 -0400

    Agregue el foreach desde el inicio para los futuros posts que se editaran

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -1,22 +1,17 @@
 <x-layout>
+	@foreach ($post as $posts)
         <article>
-            <h1>
-                @foreach ($post as $posts)
+            <h1>            
                     {{ $posts->title }}    
-                @endforeach
-                
             </h1>
             <p>
-                @foreach ($post as $posts)
-                    by <a href="#">{{$posts->user->name}}</a> <a href="/categories/{{ $posts->category->slug }}"> {{ $posts->category->name }} </a>    
-                @endforeach
-                
+                    by <a href="#">{{$posts->user->name}}</a> <a href="/categories/{{ $posts->category->slug }}"> {{ $posts->category->name }} </a>
             </p>     
             <p>
-                @foreach ($post as $posts)
                     {{ $posts->body }}    
-                @endforeach
+                
             </p>
             <a href="/">Atrás</a>
         </article>
+	@endforeach
 </x-layout>
